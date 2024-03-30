a5d063264f5bd62df61c848beb34af171954427d
Pedro Magalhães <38951956+pmags@users.noreply.github.com>
2024-03-30 10:04:43 +0000

    addede some sound

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
     receivedString = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
     if (receivedString == "0") {
         basic.showIcon(IconNames.Happy)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.UntilDone)
         pins.digitalWritePin(DigitalPin.P13, 0)
         pins.digitalWritePin(DigitalPin.P15, 0)
         pins.analogWritePin(AnalogPin.P14, 0)
@@ -31,6 +32,7 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
         pins.digitalWritePin(DigitalPin.P15, 1)
         pins.analogWritePin(AnalogPin.P14, 1000)
         pins.analogWritePin(AnalogPin.P16, 1000)
+        music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.InBackground)
     } else if (receivedString == "right") {
         basic.showLeds(`
             . . # . .
@@ -68,4 +70,6 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
 let receivedString = ""
 bluetooth.startUartService()
 basic.showIcon(IconNames.Square)
+music.setBuiltInSpeakerEnabled(true)
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
 receivedString = "0"
